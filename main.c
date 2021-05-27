d0cec434198fa976ad720ddbcb6040ba3e635f04
shubhammishra-2020 <shubham.mishra@sjsu.edu>
2021-05-27 12:03:04 -0700

    first commit

--- main.c
+++ main.c
@@ -1,1 +1,23 @@
 #include <stdio.h>
+
+
+int main() {
+    char board[9] = {'-','-','-'};
+    char input;
+    char output;
+
+    printf("%c %c %c \n", board[0], board[1], board[2]);
+    printf("%c %c %c \n", board[0], board[1], board[2]);
+    printf("%c %c %c \n", board[0], board[1], board[2]);
+
+    printf("Enter X or O: " );
+    scanf("%c", &input);
+
+    if(input == 'X'){
+        output = 'O';
+        
+    }
+    if(input == 'O'){
+        output = 'X';
+    }
+}

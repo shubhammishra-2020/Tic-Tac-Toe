d63d4e4540c5867db5109450e1f58343e1a44f62
shubhammishra-2020 <shubham.mishra@sjsu.edu>
2021-05-30 11:44:50 -0700

    added winner statement and fixed rand function overwriting input from user

--- main.c
+++ main.c
@@ -24,23 +24,29 @@ int main() {
 
             //horizontal
             if(board[0] == board[1] && board[1] == board[2]){
+                printf("%c is the winner!!!! \n", board[0]);
                 break;
             }
             else if(board[3] == board[4] && board[4] == board[5]){
+                printf("%c is the winner!!!! \n", board[3]);
                 break;
             }
 
             else if(board[6] == board[7] && board[7] == board[8]){
+                printf("%c is the winner!!!! \n", board[6]);
                 break;
             }
             //vertical
             else if(board[0] == board[3] && board[3] == board[6]){
+                printf("%c is the winner!!!! \n", board[0]);
                 break;
             }
             else if(board[1] == board[4] && board[4] == board[7]){
+                printf("%c is the winner!!!! \n", board[1]);
                 break;
             }
             else if(board[2] == board[5] && board[5] == board[8]){
+                printf("%c is the winner!!!! \n", board[2]);
                 break;
             }
         }
@@ -56,23 +62,29 @@ int main() {
 
             //horizontal
             if(board[0] == board[1] && board[1] == board[2]){
+                printf("%c is the winner!!!! \n", board[0]);
                 break;
             }
             else if(board[3] == board[4] && board[4] == board[5]){
+                printf("%c is the winner!!!! \n", board[3]);
                 break;
             }
 
             else if(board[6] == board[7] && board[7] == board[8]){
+                printf("%c is the winner!!!! \n", board[6]);
                 break;
             }
             //vertical
             else if(board[0] == board[3] && board[3] == board[6]){
+                printf("%c is the winner!!!! \n", board[0]);
                 break;
             }
             else if(board[1] == board[4] && board[4] == board[7]){
+                printf("%c is the winner!!!! \n", board[1]);
                 break;
             }
             else if(board[2] == board[5] && board[5] == board[8]){
+                printf("%c is the winner!!!! \n", board[2]);
                 break;
             }
         }
--- ocase.c
+++ ocase.c
@@ -5,7 +5,7 @@ void o(int *x, char *b, int size){
     srand((unsigned int) time(NULL));
     if(*x == 0){
             b[0] = 'O';
-            if(b[rand()%9 != 'O']){
+            if(b[rand()%9] != 'O'){
                 b[rand()%9] = 'X';
             }
             printf("%c %c %c \n", b[0], b[1], b[2]);
@@ -14,56 +14,72 @@ void o(int *x, char *b, int size){
         }
         else if(*x == 1){
             b[1] = 'O';
-            b[rand()%9] = 'X';
+            if(b[rand()%9] != 'O'){
+                b[rand()%9] = 'X';
+            }
             printf("%c %c %c \n", b[0], b[1], b[2]);
             printf("%c %c %c \n", b[3], b[4], b[5]);
             printf("%c %c %c \n", b[6], b[7], b[8]);
         }
         else if(*x== 2){
             b[2] = 'O';
-            b[rand()%9] = 'X';
+            if(b[rand()%9] != 'O'){
+                b[rand()%9] = 'X';
+            }
             printf("%c %c %c \n", b[0], b[1], b[2]);
             printf("%c %c %c \n", b[3], b[4], b[5]);
             printf("%c %c %c \n", b[6], b[7], b[8]);
         }
         else if(*x == 3){
             b[3] = 'O';
-            b[rand()%9] = 'X';
+            if(b[rand()%9] != 'O'){
+                b[rand()%9] = 'X';
+            }
             printf("%c %c %c \n", b[0], b[1], b[2]);
             printf("%c %c %c \n", b[3], b[4], b[5]);
             printf("%c %c %c \n", b[6], b[7], b[8]);
         }
         else if(*x == 4){
             b[4] = 'O';
-            b[rand()%9] = 'X';
+            if(b[rand()%9] != 'O'){
+                b[rand()%9] = 'X';
+            }
             printf("%c %c %c \n", b[0], b[1], b[2]);
             printf("%c %c %c \n", b[3], b[4], b[5]);
             printf("%c %c %c \n", b[6], b[7], b[8]);
         }
         else if(*x == 5){
             b[5] = 'O';
-            b[rand()%9] = 'X';
+           if(b[rand()%9] != 'O'){
+                b[rand()%9] = 'X';
+            }
             printf("%c %c %c \n", b[0], b[1], b[2]);
             printf("%c %c %c \n", b[3], b[4], b[5]);
             printf("%c %c %c \n", b[6], b[7], b[8]);
         }
         else if(*x== 6){
             b[6] = 'O';
-            //b[rand()%9] = 'X';
+            if(b[rand()%9] != 'O'){
+                b[rand()%9] = 'X';
+            }
             printf("%c %c %c \n", b[0], b[1], b[2]);
             printf("%c %c %c \n", b[3], b[4], b[5]);
             printf("%c %c %c \n", b[6], b[7], b[8]);
         }
         else if(*x == 7){
             b[7] = 'O';
-            b[rand()%9] = 'X';
+            if(b[rand()%9] != 'O'){
+                b[rand()%9] = 'X';
+            }
             printf("%c %c %c \n", b[0], b[1], b[2]);
             printf("%c %c %c \n", b[3], b[4], b[5]);
             printf("%c %c %c \n", b[6], b[7], b[8]);
         }
         else if(*x == 8){
             b[8] = 'O';
-            b[rand()%9] = 'X';
+            if(b[rand()%9] != 'O'){
+                b[rand()%9] = 'X';
+            }
             printf("%c %c %c \n", b[0], b[1], b[2]);
             printf("%c %c %c \n", b[3], b[4], b[5]);
             printf("%c %c %c \n", b[6], b[7], b[8]);

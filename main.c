4a36ae3947ff6b1f32a7c4279c99c72baff65b30
shubhammishra-2020 <shubham.mishra@sjsu.edu>
2021-05-30 01:30:49 -0700

    remove winner var

--- main.c
+++ main.c
@@ -6,7 +6,6 @@ int main() {
     char board[9] = {'0','1','2','3','4','5','6','7','8'};
     char input;
     int temp;
-    char winner;
 
     printf("%c %c %c \n", board[0], board[1], board[2]);
     printf("%c %c %c \n", board[3], board[4], board[5]);
@@ -22,7 +21,7 @@ int main() {
             printf("Enter location to place X: ");
             scanf("%d", &temp);
             x(&temp, board, 9);
-            
+
             //horizontal
             if(board[0] == board[1] && board[1] == board[2]){
                 break;

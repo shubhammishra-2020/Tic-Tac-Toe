c4bcfb1eaf7fa4909ba8f8dcd717e738663fdeb7
shubhammishra-2020 <shubham.mishra@sjsu.edu>
2021-05-30 00:54:10 -0700

    added rand function

--- ocase.c
+++ ocase.c
@@ -1,55 +1,67 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 void o(int *x, char b[9], int size){
+    srand((unsigned int) time(NULL));
     if(*x == 0){
             b[0] = 'O';
+            b[rand()%9] = 'X';
             printf("%c %c %c \n", b[0], b[1], b[2]);
             printf("%c %c %c \n", b[3], b[4], b[5]);
             printf("%c %c %c \n", b[6], b[7], b[8]);
         }
         else if(*x == 1){
             b[1] = 'O';
+            b[rand()%9] = 'X';
             printf("%c %c %c \n", b[0], b[1], b[2]);
             printf("%c %c %c \n", b[3], b[4], b[5]);
             printf("%c %c %c \n", b[6], b[7], b[8]);
         }
         else if(*x== 2){
             b[2] = 'O';
+            b[rand()%9] = 'X';
             printf("%c %c %c \n", b[0], b[1], b[2]);
             printf("%c %c %c \n", b[3], b[4], b[5]);
             printf("%c %c %c \n", b[6], b[7], b[8]);
         }
         else if(*x == 3){
             b[3] = 'O';
+            b[rand()%9] = 'X';
             printf("%c %c %c \n", b[0], b[1], b[2]);
             printf("%c %c %c \n", b[3], b[4], b[5]);
             printf("%c %c %c \n", b[6], b[7], b[8]);
         }
         else if(*x == 4){
             b[4] = 'O';
+            b[rand()%9] = 'X';
             printf("%c %c %c \n", b[0], b[1], b[2]);
             printf("%c %c %c \n", b[3], b[4], b[5]);
             printf("%c %c %c \n", b[6], b[7], b[8]);
         }
         else if(*x == 5){
             b[5] = 'O';
+            b[rand()%9] = 'X';
             printf("%c %c %c \n", b[0], b[1], b[2]);
             printf("%c %c %c \n", b[3], b[4], b[5]);
             printf("%c %c %c \n", b[6], b[7], b[8]);
         }
         else if(*x== 6){
             b[6] = 'O';
+            b[rand()%9] = 'X';
             printf("%c %c %c \n", b[0], b[1], b[2]);
             printf("%c %c %c \n", b[3], b[4], b[5]);
             printf("%c %c %c \n", b[6], b[7], b[8]);
         }
         else if(*x == 7){
             b[7] = 'O';
+            b[rand()%9] = 'X';
             printf("%c %c %c \n", b[0], b[1], b[2]);
             printf("%c %c %c \n", b[3], b[4], b[5]);
             printf("%c %c %c \n", b[6], b[7], b[8]);
         }
         else if(*x == 8){
             b[8] = 'O';
+            b[rand()%9] = 'X';
             printf("%c %c %c \n", b[0], b[1], b[2]);
             printf("%c %c %c \n", b[3], b[4], b[5]);
             printf("%c %c %c \n", b[6], b[7], b[8]);
--- xcase.c
+++ xcase.c
@@ -1,57 +1,72 @@
 #include <stdio.h>
+#include <time.h>
+#include<stdlib.h>
+
 void x(int *x, char b[9], int size){
+    srand((unsigned int) time(NULL));
     if(*x == 0){
             b[0] = 'X';
+            b[rand()%9] = 'O';
             printf("%c %c %c \n", b[0], b[1], b[2]);
             printf("%c %c %c \n", b[3], b[4], b[5]);
             printf("%c %c %c \n", b[6], b[7], b[8]);
         }
         else if(*x == 1){
             b[1] = 'X';
+            b[rand()%9] = 'O';
             printf("%c %c %c \n", b[0], b[1], b[2]);
             printf("%c %c %c \n", b[3], b[4], b[5]);
             printf("%c %c %c \n", b[6], b[7], b[8]);
         }
         else if(*x== 2){
             b[2] = 'X';
+            b[rand()%9] = 'O';
             printf("%c %c %c \n", b[0], b[1], b[2]);
             printf("%c %c %c \n", b[3], b[4], b[5]);
             printf("%c %c %c \n", b[6], b[7], b[8]);
         }
         else if(*x == 3){
             b[3] = 'X';
+            b[rand()%9] = 'O';
             printf("%c %c %c \n", b[0], b[1], b[2]);
             printf("%c %c %c \n", b[3], b[4], b[5]);
             printf("%c %c %c \n", b[6], b[7], b[8]);
         }
         else if(*x == 4){
             b[4] = 'X';
+            b[rand()%9] = 'O';
             printf("%c %c %c \n", b[0], b[1], b[2]);
             printf("%c %c %c \n", b[3], b[4], b[5]);
             printf("%c %c %c \n", b[6], b[7], b[8]);
         }
         else if(*x == 5){
             b[5] = 'X';
+            b[rand()%9] = 'O';
             printf("%c %c %c \n", b[0], b[1], b[2]);
             printf("%c %c %c \n", b[3], b[4], b[5]);
             printf("%c %c %c \n", b[6], b[7], b[8]);
         }
         else if(*x== 6){
             b[6] = 'X';
+            b[rand()%9] = 'O';
             printf("%c %c %c \n", b[0], b[1], b[2]);
             printf("%c %c %c \n", b[3], b[4], b[5]);
             printf("%c %c %c \n", b[6], b[7], b[8]);
         }
         else if(*x == 7){
             b[7] = 'X';
+            b[rand()%9] = 'O';
             printf("%c %c %c \n", b[0], b[1], b[2]);
             printf("%c %c %c \n", b[3], b[4], b[5]);
             printf("%c %c %c \n", b[6], b[7], b[8]);
         }
         else if(*x == 8){
             b[8] = 'X';
+            b[rand()%9] = 'O';
             printf("%c %c %c \n", b[0], b[1], b[2]);
             printf("%c %c %c \n", b[3], b[4], b[5]);
             printf("%c %c %c \n", b[6], b[7], b[8]);
         }
+
+
 }

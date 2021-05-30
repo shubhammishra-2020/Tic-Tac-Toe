ddd95fa79006da8306eab77d33fe64ad6c0c0395
shubhammishra-2020 <shubham.mishra@sjsu.edu>
2021-05-30 00:54:54 -0700

    removed output variable

--- main.c
+++ main.c
@@ -5,7 +5,6 @@
 int main() {
     char board[9] = {'0','1','2','3','4','5','6','7','8'};
     char input;
-    char output;
     int temp;
 
     printf("%c %c %c \n", board[0], board[1], board[2]);
@@ -18,7 +17,6 @@ int main() {
 
     //X Input
     if(input == 'X'){
-        output = 'O';
         for(int i=0; i<9;i++){
             printf("Enter location to place X: ");
             scanf("%d", &temp);
@@ -28,7 +26,6 @@ int main() {
 
     //O input
     if(input == 'O'){
-        output = 'X';
         for (int i=0; i<9; i++){
             printf("Enter location to place O: ");
             scanf("%d", &temp);
